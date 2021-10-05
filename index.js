67cb946cd4da202df6693e700a6a76a72bee1d45
AurionHoward <aurionhoward@gmail.com>
2021-10-04 22:33:54 -0400

    Updated Json

---
+++ index.js
@@ -0,0 +1,28 @@
+const init = () => {
+    fetch("http://localhost:3000/tracks")
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+
+}
+
+
+function topTracks(){
+
+    const albumImage = document.getElementByClassName('album-image')
+    const trackName = document.getElementsByClassName('track-name')
+    const artist = document.getElementByClassName('artist')
+    const albumName = dococument.getElementByClassName('album-name')
+    const releaseDate = document.getElementsByClassName('release-date')
+   
+
+
+
+}
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', init)
