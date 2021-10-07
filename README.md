7d74fc9669ada3a457f693f33f1f0c4722a876c5
Shannon Sorensen <sorensen.sms@gmail.com>
2021-10-07 18:36:28 -0500

    custom json

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 // As a user, I should be able to submit a review of tracks.
 
 // As a user, I can view additional details on a specific track.
+ 
+  GET https://api.spotify.com/v1/artists/{id}/top-tracks
+
+curl -X "GET" "https://api.spotify.com/v1/artists/1dfeR4HaWDbWqFHLkxsg1d/top-tracks" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQBvLHhJphx7bnpTCTk-TxPCJMZsT7-8iQqlfoQjV5oyULRZ8oiSRV_OukqXJ8KYbLf8z6WPr9wT6sOH0JxIuUGWGQt27E5kgM1qNsiR4TpFFzOIUJo1SpKWZN2epbYzrVuFe-lHQoRyrRtWNu7AdFtZGthdtHCD5f8"
---
+++ index.js
@@ -0,0 +1,53 @@
+const init = () => {
+	const token = 'BQAnXkxyZ755aAocueBBATwN-VwJJnpQZ7gfYqoLzsScUjxIpXe89RYLDXcycu0igl6sDOrXcZo_lFwfHiQrPQnILRsmSSHfU7ByRYf9_hAd7T8fFT2ix-CtCfwnfEO1DmJa5p4AaCi_QfI5uODOJzr8SGZ5pdPpX8i5YALEzg'
+	const topTrack= []
+	const init = () =>{
+	  fetch('https://api.spotify.com/v1/artists/3TVXtAsR1Inumwj472S9r4/top-tracks?market=US', {
+		headers: {
+		  'Accept': 'application/json',
+		  'Content-Type': 'application/json',
+		  'Authorization': 'Bearer ' + token   
+	  }
+	  })
+		.then(res => res.json())
+		.then(res => { topTracks(res) 
+		})
+	}
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
+    const albumImage = document.getElementByClassName('album-image')
+    const trackName = document.getElementsByClassName('track-name')
+    const artist = document.getElementByClassName('artist')
+    const albumName = dococument.getElementByClassName('album-name')
+    const releaseDate = document.getElementsByClassName('release-date')
+}
+
+function renderCourses(courses) {			
+	const coursesMenu = document.getElementById('courses');
+	coursesMenu.replaceChildren();
+
+	courses.forEach(course => {
+
+		const img = document.createElement('img');
+		img.src = course.image;
+
+	img.addEventListener("click", () => {
+		const title = document.getElementById('course_title');
+		title.textContent = course.title;
+
+		const description = document.getElementById('course_description');
+		description.textContent = course.description;
+
+		const duration = document.getElementById('course_duration');
+		duration.textContent = course.duration;
+
+		const spaces = document.getElementById('course-spaces');
+		spaces.textContent = course.capacity - course.students_enrolled;
+
+	coursesMenu.appendChild(img);
+	})
+},
+
+document.addEventListener('DOMContentLoaded', init)
